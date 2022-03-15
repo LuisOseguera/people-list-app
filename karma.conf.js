61d89be164bb8c84bfb29b5a7c75c461fd61c510
Luis Oseguera <osegueraluis436@gmail.com>
2022-03-15 15:52:52 -0600

    Changes on main project name.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/listado-personas'),
+      dir: require('path').join(__dirname, './coverage/people-list-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
